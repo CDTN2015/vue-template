f3c79ebcb8020a470219ef3e2f185384533b66f0
“CDTN2015” <1512484368@qq.com>
2022-02-23 10:41:41 +0800

    initialize

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
+    es6: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    sourceType: 'module',
     ecmaVersion: 2020,
+    parser: 'babel-eslint',
+    ecmaFeatures: 'impliedStrict',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
   },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
-  //semi: true,
+  semi: true,
   singleQuote: true,
   bracketSpacing: true,
   bracketSameLine: false,
@@ -10,4 +10,5 @@ module.exports = {
   proseWrap: 'never', //.md散文换行设置
   //已弃用：jsxBracketSameLine: false,
   trailingComma: 'es5',
+  //vueIndentScriptAndStyle: true, //在 Vue 文件中缩进脚本和样式标签
 };
